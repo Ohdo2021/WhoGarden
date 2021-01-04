ebfcb82834733a56b06fd13db734807dbea5d3e2
noion <noion0511@gmail.com>
2021-01-04 18:20:30 +0900

    채팅 레이아웃

--- WhoGarden/app/src/main/java/com/ohdo/whogarden/chatting/ChattingActivity.kt
+++ WhoGarden/app/src/main/java/com/ohdo/whogarden/chatting/ChattingActivity.kt
@@ -2,11 +2,29 @@ package com.ohdo.whogarden.chatting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.ohdo.whogarden.R
 
 class ChattingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatting)
+
+        val ChattingToolbar = findViewById(R.id.toolbar) as androidx.appcompat.widget.Toolbar
+        setSupportActionBar(ChattingToolbar)
+        val ChattingActionBar = supportActionBar!!
+        ChattingActionBar.setDisplayShowTitleEnabled(false)
+        ChattingActionBar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when(id) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
